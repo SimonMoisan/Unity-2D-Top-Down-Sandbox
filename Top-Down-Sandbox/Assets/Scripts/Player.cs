af2d7a43fca3113a7cb975734e6484c6a68b0e28
MiisterB <40004939+MiisterB@users.noreply.github.com>
2019-09-17 17:57:24 +0200

    first commit

---
+++ Top-Down-Sandbox/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movespeed = 5f;
+
+    public Rigidbody2D rb;
+
+    private Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    //Better with physic
+    void FixedUpdate()
+    {
+        //Movement
+        rb.MovePosition(rb.position + movement * movespeed * Time.fixedDeltaTime);
+    }
+}
